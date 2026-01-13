5c04ae82f7549958bda438611b1077935f80543f
Abhi <abhi13shelke@gmail.com>
2026-01-13 11:29:01 +0530

    Patterns commited

--- DSA_/src/patterns/Patterns.java
+++ DSA_/src/patterns/Patterns.java
@@ -1,5 +1,6 @@
 package patterns;
 
+import java.time.chrono.MinguoChronology;
 import java.util.Scanner;
 
 public class Patterns {
@@ -154,11 +155,168 @@ public class Patterns {
 		}
 	}
 
+	public static void print13(int n) {
+		for (int i = 0; i < n; i++) {
+			for (char j = 'A'; j <= 'A' + i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print14(int n) {
+		for (int i = 0; i < n; i++) {
+			for (char j = 'A'; j <= 'A' + (n - i - 1); j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print15(int n) {
+		for (int i = 0; i < n; i++) {
+			char ch = (char) ('A' + i);
+			for (char j = 0; j <= i; j++) {
+				System.out.print(ch + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print16(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				System.out.print(" ");
+			}
+			char ch = 'A';
+			int breankpoint = (2 * i + 1) / 2;
+			for (char j = 1; j <= 2 * i + 1; j++) {
+				System.out.print(ch);
+				if (j <= breankpoint)
+					ch++;
+				else
+					ch--;
+			}
+			for (int j = 0; j < n - i - 1; j++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print17(int n) {
+		for (int i = 0; i < n; i++) {
+			for (char j = (char) ('E' - i); j <= 'E'; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print18(int n) {
+		int iniS = 0;
+		for (int i = 0; i < n; i++) {
+			// stars
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			// spaces
+			for (int j = 0; j <= iniS; j++) {
+				System.out.print(" ");
+			}
+
+			// stars
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			iniS += 2;
+			System.out.println();
+
+		}
+		int iniS1 = 2 * n - 2;
+		for (int i = 1; i <= n; i++) {
+			// stars
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			// spaces
+			for (int j = 0; j <= iniS1; j++) {
+				System.out.print(" ");
+			}
+
+			// stars
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			iniS1 -= 2;
+			System.out.println();
+
+		}
+
+	}
+
+	public static void print19(int n) {
+		int spaces = 2 * n - 2;
+		for (int i = 1; i <= 2 * n - 1; i++) {
+			int stars = i;
+			if (i > n)
+				stars = 2 * n - i;
+			// stars
+			for (int j = 1; j <= stars; j++) {
+				System.out.print("*");
+			}
+			// spaces
+			for (int j = 1; j <= spaces; j++) {
+				System.out.print(" ");
+			}
+
+			// stars
+			for (int j = 1; j <= stars; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			if (i < n)
+				spaces -= 2;
+			else
+				spaces += 2;
+		}
+	}
+
+	public static void print20(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print21(int n) {
+		for (int i = 0; i < 2 * n - 1; i++) {
+			for (int j = 0; j < 2 * n - 1; j++) {
+				int top = i;
+				int left = j;
+				int right = (2 * n - 2) - j;
+				int down = (2 * n - 2) - i;
+				int minDist = Math.min(Math.min(top, down), Math.min(left, right));
+
+				// print the value based on distance
+				System.out.print(n - minDist + " ");
+
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		print12(n);
+		print21(n);
 
 	}
 }
