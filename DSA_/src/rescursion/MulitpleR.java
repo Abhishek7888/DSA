b4895969d93942ca3de30a4e36e71ea13b57b967
Abhi <abhi13shelke@gmail.com>
2026-01-19 22:56:29 +0530

    Multiple Recursion calls program.

---
+++ DSA_/src/rescursion/MulitpleR.java
@@ -0,0 +1,23 @@
+package rescursion;
+
+import java.util.Scanner;
+
+public class MulitpleR {
+
+	static int fibonacci(int n) {
+		if (n <= 1) {
+			return n;
+		}
+		int last = fibonacci(n - 1);
+		int slast = fibonacci(n - 2);
+
+		return last + slast;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fibonacci(n));
+
+	}
+}
