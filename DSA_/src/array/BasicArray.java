6684112b986ac36347744299e69897b174de4234
Abhi <abhi13shelke@gmail.com>
2026-02-13 12:08:57 +0530

    Basics of arrays

---
+++ DSA_/src/array/BasicArray.java
@@ -0,0 +1,45 @@
+package array;
+
+import java.util.Scanner;
+
+public class BasicArray {
+
+	// largest element in an array
+	static void findLargest(int arr[], int n) {
+		int largest = arr[0];
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > largest) {
+				largest = arr[i];
+			}
+		}
+		System.out.println("Largest Element :" + largest);
+	}
+
+	// second largest element in an array
+	static void sLargest(int arr[], int n) {
+		int largest = arr[0];
+		int sLargest = -1;
+		for (int i = 1; i < n; i++) {
+			if (arr[i] > largest) {
+				sLargest = largest;
+				largest = arr[i];
+			} else if (arr[i] < largest && arr[i] > sLargest) {
+				sLargest = arr[i];
+			}
+		}
+		System.out.println("Second Largest Element : " + sLargest);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of elements: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter array elements:");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sLargest(arr, n);
+
+	}
+}
