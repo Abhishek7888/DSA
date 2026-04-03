01d5b81642302a45b229357e8c9efe86d5d5401c
Abhi <abhi13shelke@gmail.com>
2026-04-03 10:04:38 +0530

    read write locks

--- Java/src/multithreading/ReadWriteCounter.java
+++ Java/src/multithreading/ReadWriteCounter.java
@@ -1,6 +1,5 @@
 package multithreading;
 
-import java.lang.ProcessBuilder.Redirect;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -28,5 +27,53 @@ public class ReadWriteCounter {
 		} finally {
 			readLock.unlock();
 		}
+	}
+
+	public static void main(String[] args) {
+
+		ReadWriteCounter counter = new ReadWriteCounter();
+
+		Runnable readTask = new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 10; i++) {
+					System.out.println(Thread.currentThread().getName() + " read : " + counter.getCount());
+				}
+			}
+		};
+
+		Runnable writeTask = new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 10; i++) {
+					counter.increament();
+					System.out.println(Thread.currentThread().getName() + " incremented ");
+				}
+			}
+		};
+
+		Thread writerThread = new Thread(writeTask);
+		Thread readerThread1 = new Thread(readTask);
+		Thread readerThread2 = new Thread(readTask);
+
+		writerThread.start();
+		readerThread1.start();
+		readerThread2.start();
+
+		try {
+			writerThread.join();
+			readerThread1.join();
+			readerThread2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Final count : " + counter.getCount());
+
 	}
 }
