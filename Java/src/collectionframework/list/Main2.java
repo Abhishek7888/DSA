fb9bdf1b78b1108ed151c4b8f474f845b5404936
Abhi <abhi13shelke@gmail.com>
2026-04-11 19:33:45 +0530

    ArrayList added

--- Java/src/collectionframework/list/Main2.java
+++ Java/src/collectionframework/list/Main2.java
@@ -1,6 +1,8 @@
 package collectionframework.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 class Student {
@@ -45,8 +47,24 @@ public class Main2 {
 		List<Student> list = new ArrayList<>();
 		list.add(new Student("Sai", 7.8));
 		list.add(new Student("Abhi", 7.5));
-		list.add(new Student("Luffy", 7.6));
-		list.sort((a, b) -> a.getGpa() - b.getGpa());
+		list.add(new Student("Luffy", 7.5));
+
+		Comparator<Student> comparator = Comparator.comparing(Student::getGpa).reversed()
+				.thenComparing(Student::getName);
+
+		list.sort(comparator);
+
+		list.sort((a, b) -> {
+			if (b.getGpa() - a.getGpa() > 0) {
+				return 1;
+			} else if (a.getGpa() - b.getGpa() < 0) {
+				return -1;
+			} else {
+				return a.getName().compareTo(b.getName());
+			}
+		});
+		
+		Collections.sort(list,comparator);
 		for (Student student : list) {
 			System.out.println(student.getName() + ":" + student.getGpa());
 		}
