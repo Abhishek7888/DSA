7bdfa2f95b5a4aa080ebb3eb392c256607d47b10
Abhi <abhi13shelke@gmail.com>
2026-04-18 10:37:31 +0530

    Vector

---
+++ Java/src/collectionframwork/vector/VectorDemo.java
@@ -0,0 +1,32 @@
+package collectionframwork.vector;
+
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		Vector<Integer> vector = new Vector<>(5);
+		vector.add(1);
+		vector.add(1);
+		vector.add(1);
+		vector.add(1);
+		vector.add(1);
+		vector.add(1);
+		System.out.println(vector.capacity());
+		LinkedList<Integer> inList = new LinkedList<>();
+		inList.add(10);
+		inList.add(20);
+		inList.add(30);
+		inList.add(40);
+		Vector<Integer> vector2 = new Vector<>(inList);
+		System.out.println(vector2); 
+		System.out.println(vector2.size());
+		for (Integer integer : vector2) {
+			System.out.println(integer);
+		}
+		
+
+	}
+
+}
